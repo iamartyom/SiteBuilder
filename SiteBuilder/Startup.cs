dfd051aee36e9b1a67e1abd3660161ec7bdb1cdc
Artem <postagric@gmail.com>
2016-07-28 20:28:10 +0300

    First commit

---
+++ SiteBuilder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SiteBuilder.Startup))]
+namespace SiteBuilder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
