239191d100133fb2d8155ce9dbda0fe21d1a76d8
Artem <postagric@gmail.com>
2016-07-29 22:01:06 +0300

    A

--- SiteBuilder/Controllers/AccountController.cs
+++ SiteBuilder/Controllers/AccountController.cs
@@ -9,10 +9,12 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using SiteBuilder.Models;
+using SiteBuilder.Filters;
 
 namespace SiteBuilder.Controllers
 {
     [Authorize]
+    [Culture]
     public class AccountController : Controller
     {
         private ApplicationSignInManager _signInManager;
