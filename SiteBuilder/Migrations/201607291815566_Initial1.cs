6265cdfc9785d63905d4e2a88eeb91af7ddef980
O.Yatsevich <qualmish@gmail.com>
2016-07-29 23:32:09 +0300

    Navbar fix

---
+++ SiteBuilder/Migrations/201607291815566_Initial1.cs
@@ -0,0 +1,27 @@
+namespace SiteBuilder.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Sites",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Sites");
+        }
+    }
+}
