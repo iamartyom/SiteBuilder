37cd6a9543513398dab1c1183d02e496ec60275d
Artem <postagric@gmail.com>
2016-08-06 18:20:51 +0300

    123

--- SiteBuilder/App_Start/BundleConfig.cs
+++ SiteBuilder/App_Start/BundleConfig.cs
@@ -11,9 +11,6 @@ namespace SiteBuilder
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
-                        "~/Scripts/jquery-ui.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
