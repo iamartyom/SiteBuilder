7cf796e4add0c43e092e79cfa17ff586d9fe4be6
Artem <postagric@gmail.com>
2016-08-06 17:59:04 +0300

    Small add

--- SiteBuilder/App_Start/BundleConfig.cs
+++ SiteBuilder/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace SiteBuilder
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
