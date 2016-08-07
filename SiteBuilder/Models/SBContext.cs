ae7742f1ffe72011e787907f7fc893a794052bd9
O.Yatsevich <qualmish@gmail.com>
2016-08-07 20:14:50 +0300

    Trying to figure out problem with templates.

---
+++ SiteBuilder/Models/SBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SiteBuilder.Models
+{
+    public class SBContext : DbContext
+    {
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Site> Sites { get; set; }
+        public DbSet<TypeMenu> TypeMenus { get; set; }
+    }
+}
