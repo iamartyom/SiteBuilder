64ed7f66087704ff1a0acdc726827337f6bb487c
Artem <postagric@gmail.com>
2016-08-07 22:23:38 +0300

    image

--- SiteBuilder/Scripts/DragNDrop.js
+++ SiteBuilder/Scripts/DragNDrop.js
@@ -3,7 +3,10 @@
 
     $('#droppable1, #droppable2, #droppable3, #droppable4').droppable({
         drop: function () {
-            alert($(this).attr('id'));
+            $(this).empty();
+            var img = '<img src="http://minionomaniya.ru/wp-content/uploads/2016/01/%D0%9A%D0%B5%D0%B2%D0%B8%D0%BD.jpg" width="' + $(this).width() + '" />'
+            $(this).html(img);
+            $(this).height($(this).find('img').height());
         },
     });
 });
