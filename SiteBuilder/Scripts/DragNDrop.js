8b68a315e0da16e67dc91cfe0ace12ff7b5e1435
Artem <postagric@gmail.com>
2016-08-07 21:47:52 +0300

    144

---
+++ SiteBuilder/Scripts/DragNDrop.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $('#draggable').draggable();
+
+    $('#droppable1, #droppable2, #droppable3, #droppable4').droppable({
+        drop: function () {
+            alert($(this).attr('id'));
+        },
+    });
+});
---
+++ SiteBuilder/Scripts/loadTemplate.js
@@ -0,0 +1,11 @@
+﻿function loadTemplate(e) {
+    $.ajax({
+        type: 'POST',
+        url: "/SiteBuilder/LoadTemplate",
+        data: { nameTemplate: e.id },
+        success: function (data) {
+            $("#layout").empty();
+            $("#layout").html(data);
+        }
+    });
+}
