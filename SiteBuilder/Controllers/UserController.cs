a97242a5afdcc6079fa1521ae894c7f6c4f82edc
Artem <postagric@gmail.com>
2016-08-13 19:56:38 +0300

    Added user profile and fix print last create site on main page

---
+++ SiteBuilder/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using SiteBuilder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteBuilder.Controllers
+{
+    public class UserController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: User
+        public ActionResult Info(string user)
+        {
+            if (db.Users.Any(c => c.UserName == user))
+            {
+                var model = new UserInfo
+                {
+                    Profile = db.Users.Where(c => c.UserName == user).First(),
+                    Sites = db.Sites.OrderByDescending(c => c.Id).Select(c => new ShowSite { NameSite = c.Name, NameUser = c.User.UserName, Description = c.Description, NamePage = c.Pages.FirstOrDefault().Name }).ToList()
+            };
+
+                return View(model);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
---
+++ SiteBuilder/Models/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiteBuilder.Models
+{
+    public class UserInfo
+    {
+        public ApplicationUser Profile { get; set; }
+        public IEnumerable<ShowSite> Sites { get; set; }
+    }
+}
