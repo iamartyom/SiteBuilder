3c0f763a9a78c19a8e7819c6d73affd548d7a61d
Artem <postagric@gmail.com>
2016-08-18 17:30:44 +0300

    Fix edit panel site in profile

--- SiteBuilder/App_Start/BundleConfig.cs
+++ SiteBuilder/App_Start/BundleConfig.cs
@@ -39,6 +39,9 @@ namespace SiteBuilder
             bundles.Add(new ScriptBundle("~/bundles/showdown").Include(
                         "~/Scripts/showdown.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/editsitepanel").Include(
+                        "~/Scripts/editSitePanel.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ SiteBuilder/Scripts/editSitePanel.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+                $('.userSite').mouseover(function () {
+                    loadEditPanel(this);
+                });
+            });
+
+            function loadEditPanel(element) {
+                $.ajax({
+                    type: 'POST',
+                    url: "/User/LoadEditPanel/",
+                    data: {
+                        nameSite: $(element).children(':first').attr('id'),
+                        user : $(element).children(':first').attr('class')
+                    },
+                    success: function (data) {
+                        $('.editPanel').empty();
+                        $(element).after(data);
+                    }
+                });
+            }
